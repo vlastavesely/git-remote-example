6702f178ecc498711ee756099401b78400ea86f2
Vlasta Vesely <vlastavesely@protonmail.ch>
2018-07-02 18:27:11 +0200

    add push-object.rb

---
+++ push-object.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+
+require 'fileutils'
+require 'zlib'
+
+# Pushes one Git object to a remote repository.
+#
+# This function dumps an object identified by the given SHA1 hash
+# and saves it into the directory specified in the 'remote' argument.
+#
+# Usualy, the local objects are stored as separate files in the
+# '.git/objects/' directory. But once th `git gc` has been called,
+# the files may get packed into a single pack file and be removed
+# from their original location. This is why we refuse to just read
+# local files and why we rather ask git to dump these for us.
+#
+def push_object(sha, remote)
+
+  type = `git cat-file -t #{sha}`.strip! # = (commit | tree | blob)
+  content = `git cat-file #{type} #{sha}`
+
+  # https://git-scm.com/book/en/v2/Git-Internals-Git-Objects
+  header = type + ' ' + content.bytesize.to_s + "\0"
+  store = header + content
+
+  FileUtils.mkdir_p(remote + '/obj/')
+  File.open(remote + '/obj/' + sha, 'wb') do |file|
+    file.write(store)
+  end
+
+end
