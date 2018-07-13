756092e99d4238cb30b0454f1515370ee9b4f2af
Vlasta Vesely <vlastavesely@protonmail.ch>
2018-07-13 13:33:02 +0200

    add test for cloning

---
+++ tests/clone.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+testsdir="$(dirname "$(readlink -f $0)")"
+
+. $testsdir/bootstrap
+
+
+# Create initial repository
+git -C $PRIMARY init >/dev/null
+git -C $PRIMARY remote add origin example://$REMOTE
+echo "/test" >$PRIMARY/.gitignore
+git -C $PRIMARY add .gitignore >/dev/null
+git -C $PRIMARY commit -m "initial" >/dev/null
+echo "GPL-2" >$PRIMARY/COPYING
+git -C $PRIMARY add COPYING >/dev/null
+git -C $PRIMARY commit -m "add COPYING" >/dev/null
+git -C $PRIMARY tag v1.0 -a -m "released v1.0"
+
+
+git -C $PRIMARY push origin master --tags
+git -C $SECONDARY clone example://$REMOTE .
